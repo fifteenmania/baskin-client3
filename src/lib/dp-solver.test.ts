2e65d35e46133d80ca407f9049f4ef19a298ea43
fifteenmania <ktthee1995@gmail.com>
2024-01-04 00:17:52 +0900

    feat: implemet algorithm

--- src/lib/dp-solver.test.ts
+++ src/lib/dp-solver.test.ts
@@ -1,3 +1,97 @@
-import { describe } from 'node:test'
+import { describe, test, expect } from '@jest/globals'
+import { DpSolver } from './dp-solver'
 
-describe('dp-solver', () => {})
+describe('dp-solver', () => {
+  test('initializes', () => {
+    const dpSolver = new DpSolver()
+    expect(dpSolver).toBeTruthy()
+  })
+  test('2player, max 2, end in 1', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(2, 2, 1)
+    expect(loseProbMat).toEqual([[1, 0]])
+  })
+  test('2player, max 2, end in 2', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(2, 2, 2)
+    expect(loseProbMat).toEqual([
+      [1, 0],
+      [0, 1],
+    ])
+  })
+  test('2player, max 2, end in 3', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(2, 2, 3)
+    expect(loseProbMat).toEqual([
+      [1, 0],
+      [0, 1],
+      [1, 0],
+    ])
+  }),
+    test('2player, max 2, end in 4', () => {
+      const dpSolver = new DpSolver()
+      const loseProbMat = dpSolver.getLoseProbMatrix(2, 2, 4)
+      expect(loseProbMat).toEqual([
+        [1, 0],
+        [0, 1],
+        [1, 0],
+        [1, 0],
+      ])
+    })
+  test('2player, max 2, end in 5', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(2, 2, 5)
+    expect(loseProbMat).toEqual([
+      [1, 0],
+      [0, 1],
+      [1, 0],
+      [1, 0],
+      [0, 1],
+    ])
+  })
+  test('2player, max 3, end in 1', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(2, 3, 1)
+    expect(loseProbMat).toEqual([[1, 0]])
+  })
+  test('2player, max 3, end in 2', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(2, 3, 2)
+    expect(loseProbMat).toEqual([
+      [1, 0],
+      [0, 1],
+    ])
+  })
+  test('3player, end in 1', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(3, 2, 1)
+    expect(loseProbMat).toEqual([[1, 0, 0]])
+  })
+  test('3player, end in 2', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(3, 2, 2)
+    expect(loseProbMat).toEqual([
+      [1, 0, 0],
+      [0, 1, 0],
+    ])
+  })
+  test('3player, end in 3', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(3, 2, 3)
+    expect(loseProbMat).toEqual([
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1],
+    ])
+  })
+  test('3player, end in 4', () => {
+    const dpSolver = new DpSolver()
+    const loseProbMat = dpSolver.getLoseProbMatrix(3, 2, 4)
+    expect(loseProbMat).toEqual([
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1],
+      [1 / 2, 0, 1 / 2],
+    ])
+  })
+})
--- src/lib/dp-solver.ts
+++ src/lib/dp-solver.ts
@@ -2,27 +2,45 @@ export class DpSolver {
   constructor() {}
 
   /**
-   * Get the probability of 1-winning for each player
+   * Get the probability of losing(1-winning) for each player
    * @param numPlayer
    * @param maxCall
    * @param numEnd
-   * @returns
+   * @returns 2D array of probability of losing
    */
   getLoseProbMatrix(
     numPlayer: number,
     maxCall: number,
     numEnd: number,
   ): number[][] {
-    const loseProbMat = []
-    const initialRow = Array.from({ length: numPlayer }, idx => {
-      if (idx === 0) {
-        return 1
-      }
-      return 0
-    })
+    const loseProbMat: number[][] = []
+    const initialRow = Array(numPlayer).fill(0)
+    initialRow[0] = 1
     loseProbMat.push(initialRow)
-    for (let row = numEnd - 1; row >= 0; row--) {
-      const newRow = []
+    for (let row = 1; row < numEnd; row++) {
+      const newRow = Array(numPlayer).fill(0)
+      const loopupStartIdx = Math.max(0, row - maxCall)
+      // find min of the first column
+      let minOfFirstCol = Infinity
+      let minRowCount = 0
+      for (let minRow = loopupStartIdx; minRow < row; minRow++) {
+        if (minOfFirstCol > loseProbMat[minRow][0]) {
+          minOfFirstCol = loseProbMat[minRow][0]
+          minRowCount = 1
+        } else if (minOfFirstCol === loseProbMat[minRow][0]) {
+          minRowCount++
+        }
+      }
+
+      for (let minRow = loopupStartIdx; minRow < row; minRow++) {
+        if (minOfFirstCol === loseProbMat[minRow][0]) {
+          for (let col = 0; col < numPlayer; col++) {
+            newRow[(col + 1) % numPlayer] +=
+              loseProbMat[minRow][col] / minRowCount
+          }
+        }
+      }
+      loseProbMat.push(newRow)
     }
     return loseProbMat
   }
